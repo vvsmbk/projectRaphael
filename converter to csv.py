f7e17c6216288887c0ee991dd47d28c21575222f
vvsmbk <samburskyvladimir@gmail.com>
2026-01-04 13:16:44 +0100

    All separated files joined in ipynb file. All the raw data moved to created InfluxDB Backup folder

--- converter to csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import pyarrow.parquet as pq
-from pathlib import Path
-
-in_path = Path("./Desktop/influx backup/saw.backup.parquet")
-df = pq.read_table(in_path).to_pandas()
-
-df.to_csv("./Desktop/influx backup/saw.csv", index=False) 
