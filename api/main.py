9ff0f432b1468a1e2ffdbdf693f05273e69f3e74
vvsmbk <samburskyvladimir@gmail.com>
2026-01-11 18:16:20 +0100

    Latest

---
+++ api/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}#
+
+@app.post("/")
+async def root():
+    return {"message": "Hello World"}#
